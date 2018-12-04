43e98b65721395ffd4b22a427ffc47dc52fc8310
Alex <sumy.ua@gmail.com>
2018-12-04 11:28:19 +0200

    Get list of coins

--- src/main/java/net/asasha/cryptoexplorer/Main.java
+++ src/main/java/net/asasha/cryptoexplorer/Main.java
@@ -1,8 +1,28 @@
 package net.asasha.cryptoexplorer;
 
+import net.asasha.cryptoexplorer.utils.JsonReader;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to the cryptoexplorer!");
+    public static void main(String[] args) throws IOException {
+        JSONObject coins = JsonReader.readJsonFromUrl("http://whattomine.com/coins.json").getJSONObject("coins");
+        Iterator<String> keys = coins.keys();
+
+        List<JSONObject> list = new ArrayList<>();
+
+        int i = 0;
+        while(keys.hasNext()) {
+            String key = keys.next();
+            JSONObject nextCoin = coins.getJSONObject(key);
+            list.add(nextCoin);
+            String tag = nextCoin.getString("tag");
+            System.out.printf("%d\t%s (%s)%n", ++i, key, tag);
+        }
     }
 }
